e64a616f2e58323c7f923f820c42e7b111f6968f
Richard Copestake <mail@richardcopestake.co.uk>
2015-01-05 15:13:00 +0000

    first commit

---
+++ parts/my-off-canvas-menu.php
@@ -0,0 +1,8 @@
+<aside class="left-off-canvas-menu" aria-hidden="true">
+   <ul id="menu-main-navigation" class="off-canvas-list">         
+              <li><a href="#">home</a></li>
+                <li><a href="#">productions</a></li> 
+                <li><a href="#">biography</a></li>
+                <li><a href="#">gallery</a></li>
+            </ul></aside>
+</aside>
---
+++ parts/my-top-bar.php
@@ -0,0 +1,13 @@
+<div class="top-bar-container contain-to-grid show-for-medium-up" role="navigation">
+    <nav class="top-bar" data-topbar="">
+        
+        <section class="top-bar-section">
+           <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">productions</a></li> 
+                <li><a href="#">biography</a></li>
+                <li><a href="#">gallery</a></li>
+           
+        </ul>    
+    </nav>
+</div>
